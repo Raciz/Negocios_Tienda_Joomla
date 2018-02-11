6419eae64a94c6231d0344f95bda5a12d6453406
Raciz <1530071@upv.edu.mx>
2018-02-11 06:58:45 +0000

    inalacion joomla

---
+++ html/administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,4 @@
+#
+#<?php die("Forbidden."); ?>
+
+2018-02-11 06:10:44 ERROR vmError: Warning, the Safe Path is <b>not accessible</b> (does not exist or no permission). For safety reasons it is very important to create a folder in a path not accessible by an URL or unguessable name, create also a folder 'invoices' and 'keys' in it to store your sensitive data secure. Our suggested path is for your system '/var/www/vmfiles/'. <br>You can also use complex folder name as 'password' for example '/var/www/html/administrator/components/com_virtuemart/7tE9ifFv4T1c/'. Use this link to the <a href='http://138.197.107.123/administrator/index.php?option=com_virtuemart&view=config' >config</a>
---
+++ html/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-02-11 06:43:05 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-02-11T06:43:05+00:00	INFO 187.198.158.184	joomlafailure	Username and password do not match or you do not have an account yet.
