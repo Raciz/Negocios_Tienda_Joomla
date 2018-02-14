a567ee5ed3e52423ae94984a0e07f242f60d12d2
Raciz <1530071@upv.edu.mx>
2018-02-14 04:26:06 +0000

    Antes de que algo salga mal

--- html/administrator/logs/error.php
+++ html/administrator/logs/error.php
@@ -7,3 +7,8 @@
 2018-02-11T06:43:05+00:00	INFO 187.198.158.184	joomlafailure	Username and password do not match or you do not have an account yet.
 2018-02-11T07:07:18+00:00	INFO 187.198.158.184	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
 2018-02-11T07:07:27+00:00	INFO 187.198.158.184	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-02-11T17:53:38+00:00	INFO 187.198.158.184	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-02-11T17:54:12+00:00	INFO 187.198.158.184	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-02-11T17:54:28+00:00	INFO 187.198.158.184	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-02-11T17:54:54+00:00	INFO 187.198.158.184	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2018-02-13T15:57:15+00:00	INFO 200.95.170.93	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
